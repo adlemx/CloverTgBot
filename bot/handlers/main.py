bebcdca6210ade06b1951e48e737aaf0ca3bff4a
Adlemex <alex963.ser@gmail.com>
2022-09-24 16:55:37 +0300

    Basic functions added

--- bot/handlers/main.py
+++ bot/handlers/main.py
@@ -1,15 +1,16 @@
-from aiogram import Dispatcher
+from aiogram import Dispatcher, Bot
 
 from bot.handlers.admin import register_admin_handlers
 from bot.handlers.other import register_other_handlers
 from bot.handlers.user import register_user_handlers
 
 
-def register_all_handlers(dp: Dispatcher) -> None:
+def register_all_handlers(dp: Dispatcher, bot: Bot) -> None:
     handlers = (
-        register_user_handlers,
         register_admin_handlers,
         register_other_handlers,
     )
+
+    register_user_handlers(dp, bot)
     for handler in handlers:
         handler(dp)
--- bot/handlers/user/main.py
+++ bot/handlers/user/main.py
@@ -1,7 +1,43 @@
-from aiogram import Dispatcher
+from aiogram import Dispatcher, types, Bot
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 
-def register_user_handlers(dp: Dispatcher):
+# ReplyKeyboardMarkup(resize_keyboard=True).add(
+#             KeyboardButton('Отправить свой контакт ☎️', request_contact=True)
+#         ).add(
+#             KeyboardButton('Отправить свою локацию 🗺️', request_location=True)
+#         )
+
+
+
+def register_user_handlers(dp: Dispatcher, bot: Bot):
     # todo: register all user handlers
-    pass
+    @dp.message_handler(commands=['start', 'help'])
+    async def send_welcome(message: types.Message):
+        await message.answer("С помощью этого бота вы сможете заказать доставку дроном \n"
+                             "для заказа напишите /order")
+
+    @dp.message_handler(commands=['order'])
+    async def start_order(message: types.Message):
+        markup_request = InlineKeyboardMarkup(row_width=1)\
+            .add(InlineKeyboardButton('1', callback_data='product1'))\
+            .add(InlineKeyboardButton('2', callback_data='product2'))\
+            .add(InlineKeyboardButton('3', callback_data='product3'))
+        await message.answer("Выберите товар который вы хотите заказать", reply_markup=markup_request)
+
+    @dp.callback_query_handler(lambda c: c.data and c.data.startswith('product'))
+    async def process_callback_preorder(callback_query: types.CallbackQuery):
+        code = callback_query.data[-1]
+        if code.isdigit(): code = int(code)
+        markup_request = InlineKeyboardMarkup(row_width=1)\
+            .add(InlineKeyboardButton('Оплатить', callback_data=f'order{code}'))
+        await bot.edit_message_text(f"Ваш заказ:\n"
+                                    f"Товар: {code}\n"
+                                    f"Стоимость: {code*10}", message_id=callback_query.message.message_id, chat_id=callback_query.message.chat.id,
+                                    reply_markup=markup_request)
+
+    @dp.callback_query_handler(lambda c: c.data and c.data.startswith('order'))
+    async def process_callback_preorder(callback_query: types.CallbackQuery):
+        code = callback_query.data[-1]
+        if code.isdigit(): code = int(code)
 
