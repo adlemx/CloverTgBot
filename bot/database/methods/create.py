fec0faef4afd2fd1fe8a779d221fef9a2588dbf6
Alexer-glitch <alexey.sergeev.8@gmai.com>
2022-09-25 20:14:45 +0300

    Db.create returns models

--- bot/database/methods/create.py
+++ bot/database/methods/create.py
@@ -1,11 +1,15 @@
 import sqlite3
 
+from bot.database.models.order import Order
+from bot.database.models.user import User
 
-def create_user(tg_id: int):
+
+def create_user(tg_id: int) -> User:
     try:
         sqlite_connection = sqlite3.connect('bot/database/db.sqlite3')
         cursor = sqlite_connection.cursor()
         cursor.execute(f"INSERT INTO users VALUES(0, {tg_id})")
+        return User(tg_id, 0)
         sqlite_connection.commit()
         cursor.close()
 
@@ -15,15 +19,15 @@ def create_user(tg_id: int):
         if (sqlite_connection):
             sqlite_connection.close()
 
-def create_order(tg_id: int, address: str, comment: str, phone: str, product: int):
+def create_order(tg_id: int, address: str, comment: str, phone: str, product: int) -> Order:
     try:
         sqlite_connection = sqlite3.connect('bot/database/db.sqlite3')
         cursor = sqlite_connection.cursor()
-        cursor.execute(f"INSERT INTO orders (address, comment, phone, product, state, tg_id) "
+        d = cursor.execute(f"INSERT INTO orders (address, comment, phone, product, state, tg_id) "
                        f"VALUES('{address}', '{comment}', '{phone}', {product}, 0, {tg_id})")
-        cursor.execute()
-        print(cursor.fetchone())
+        print(d.lastrowid)
         sqlite_connection.commit()
+        return Order(d.lastrowid, product, tg_id, address, comment, phone, 0)
         cursor.close()
 
     except sqlite3.Error as error:
--- bot/database/models/order.py
+++ bot/database/models/order.py
@@ -3,14 +3,17 @@ import types
 
 class Order(object):
     order_id: int
-    product: str
+    product: int
     comment: str
     address: str = ""
     phone: str = ""
     state: int = 0 # 0 - pre order; 1 - payment required; 2 - assembling; 3 - delivery; 4 - done;
     tg_id: int = 0
 
-    def __init__(self, orders: int, tg_id: int, address: str, phone: str, state: int):
+    def __init__(self, order_id: int, product: int, tg_id: int, address: str,  comment: str, phone: str, state: int):
+        self.order_id = order_id
+        self.product = product
+        self.comment = comment
         self.tg_id = tg_id
         self.address = address
         self.phone = phone
